0610669ea4e2f78a199b2c4affcbc7e4658b106a
lghiggino <lghiggino@gmail.com>
2020-12-28 15:44:23 -0300

    rotating navigation completed

--- 03 - Rotating Navigation/js/rotating-navigation.js
+++ 03 - Rotating Navigation/js/rotating-navigation.js
@@ -1,1 +1,41 @@
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container")
+
+open.addEventListener("click", () =>{
+    container.classList.add("show-nav")
+})
+
+close.addEventListener("click", () =>{
+    container.classList.remove("show-nav")
+})
+
+//click on the navigation, transform the container back, send to the link
+const links = document.querySelectorAll(".links")
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        console.log(link.id)
+        if (container.classList.contains("show-nav")){
+            container.classList.remove("show-nav")
+        }
+
+        function linkFinder (){
+            switch(link.id){
+                case ("index"):
+                    window.location.href="index.html";
+                    break;
+                case("about"):
+                    window.location.href="about.html";
+                    break;
+                case("contact"):
+                    window.location.href="contact.html";
+                    break;
+            }
+        }
+        setTimeout(linkFinder, 500)
+    })
+})
+
+
 
