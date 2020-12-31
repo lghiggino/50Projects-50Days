b1d417957018baad32ae587388cd8a7290f4a47b
lghiggino <lghiggino@gmail.com>
2020-12-30 22:45:09 -0300

    scroll division correction

--- 06-Scroll_Animation/js/main.js
+++ 06-Scroll_Animation/js/main.js
@@ -7,7 +7,7 @@ window.addEventListener("scroll", checkBoxes)
 checkBoxes()
 
 function checkBoxes(){
-    const triggerBottom = window.innerHeight / 6 * 4;
+    const triggerBottom = window.innerHeight / 5 * 4;
     boxes.forEach(box => {
         const boxTop = box.getBoundingClientRect().top;
         
