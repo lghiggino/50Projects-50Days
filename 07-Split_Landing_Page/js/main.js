6ca9a504ba40b395653cba28ca16a88c616524d7
lghiggino <lghiggino@gmail.com>
2020-12-31 12:09:45 -0300

    expanded split landing page

--- 07-Split_Landing_Page/js/main.js
+++ 07-Split_Landing_Page/js/main.js
@@ -1,1 +1,15 @@
+const container = document.querySelector(".container");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const leftButton = document.querySelector("#button-left");
+const rightButton = document.querySelector("#button-right");
 
+left.addEventListener("mouseenter", () => container.classList.add("hover-left"));
+left.addEventListener("mouseleave", () => container.classList.remove("hover-left"));
+
+right.addEventListener("mouseenter", () => container.classList.add("hover-right"));
+right.addEventListener("mouseleave", () => container.classList.remove("hover-right"));
+
+
+leftButton.addEventListener("click", () => container.classList.add("hover-left"));
+rightButton.addEventListener("click", () => container.classList.add("hover-right"));
