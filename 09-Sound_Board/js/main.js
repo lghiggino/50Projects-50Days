d9e118f78507c53f45955be35868d50ab9550174
lghiggino <lghiggino@gmail.com>
2021-01-02 15:43:29 -0300

    sound board is done

--- 09-Sound_Board/js/main.js
+++ 09-Sound_Board/js/main.js
@@ -1,1 +1,26 @@
+const sounds = document.querySelectorAll("audio");
 
+console.log(sounds);
+
+sounds.forEach(sound => {
+    const btn = document.createElement("button");
+    btn.classList.add("btn")
+
+    btn.innerText = sound.id
+    btn.addEventListener("click", () => {
+        stopSongs()
+        document.getElementById(sound.id).play()
+    })
+
+
+
+    document.getElementById("buttons").appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound.id);
+        song.pause();
+        song.currentTime = 0;
+    })
+}
