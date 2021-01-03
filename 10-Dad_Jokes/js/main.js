e2e610949d84a6a298adc191d9e89ccc79905324
lghiggino <lghiggino@gmail.com>
2021-01-03 17:45:33 -0300

    Dad jokes is done

--- 10-Dad_Jokes/js/main.js
+++ 10-Dad_Jokes/js/main.js
@@ -1,1 +1,23 @@
+const jokeEl = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
+generateJoke()
+
+async function generateJoke(){
+    const config = {
+        headers: {"Accept": "application/json"}
+    }
+
+    const response = await fetch("https://icanhazdadjoke.com", config); 
+    const data = await response.json();
+    jokeEl.innerHTML = data.joke;
+}
+
+
+jokeBtn.addEventListener("click", generateJoke);
+
+// then method
+// .then(res=> res.json())
+// .then(data => {
+//     jokeEl.innerHTML = data.joke;
+// })
