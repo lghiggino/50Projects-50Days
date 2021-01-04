95e63b6c58861dfc02737230a973079706209af7
lghiggino <lghiggino@gmail.com>
2021-01-04 16:47:09 -0300

    event key codes is done

--- 11-Event_KeyCodes/js/main.js
+++ 11-Event_KeyCodes/js/main.js
@@ -1,1 +1,27 @@
+const keys = document.getElementById("keys")
 
+window.addEventListener("keydown" , (e) => {
+    console.log(e.key, e.keyCode, e.code)
+    keys.innerHTML = `
+        <div class="key">
+            ${e.key === " " ? "Space" : e.key}
+            <small>event.key</small>
+        </div>
+
+        <div class="key">
+            ${e.keyCode}
+            <small>event.keyCode</small>
+        </div>
+
+        <div class="key">
+            ${e.code}
+            <small>event.code</small>
+        </div>
+    `
+
+
+
+    if (e.key === " "){
+
+    }
+})
