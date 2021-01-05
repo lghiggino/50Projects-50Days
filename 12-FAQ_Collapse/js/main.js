dee10a1bb59c5e7c6c6dd6822111ce8bab1425c6
lghiggino <lghiggino@gmail.com>
2021-01-05 16:03:14 -0300

    FAQ Collapse is done; back to OOP

--- 12-FAQ_Collapse/js/main.js
+++ 12-FAQ_Collapse/js/main.js
@@ -1,1 +1,23 @@
+const toggles = document.querySelectorAll(".faq-toggle");
 
+toggles.forEach(toggle => {
+    toggle.addEventListener("click", () => {
+        toggle.parentNode.classList.toggle("active")
+    })
+})
+
+// const chevron = document.querySelectorAll(".fa-chevron-down");
+// const times = document.querySelectorAll(".fa-times");
+
+// chevron.forEach(chev => {
+//     chev.addEventListener("click", (e) => {
+//         console.log(e)
+//         e.target.parentNode.parentNode.classList.add("active")
+//     })
+// })
+
+// times.forEach(cross => {
+//     cross.addEventListener("click", (e) => {
+//         e.target.parentNode.parentNode.classList.remove("active")
+//     })
+// })
