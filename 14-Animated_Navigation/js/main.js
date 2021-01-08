d9d9b2a649c357f052f7e22f3a464221469422b3
lghiggino <lghiggino@gmail.com>
2021-01-08 11:58:23 -0300

    animated navigation on index

--- 14-Animated_Navigation/js/main.js
+++ 14-Animated_Navigation/js/main.js
@@ -1,1 +1,13 @@
+const button = document.querySelector("#toggle");
+const nav = document.querySelector("#nav");
+const links = document.querySelectorAll("li");
 
+
+links.forEach(link => {
+    link.addEventListener("click", toggleMenu)
+})
+button.addEventListener("click", toggleMenu);
+
+function toggleMenu(){
+    nav.classList.toggle("active")
+}
