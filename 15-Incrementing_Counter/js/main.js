610c070132de70024715fd831c81379dcb89ec97
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-01-10 16:25:21 -0300

    incrementing counter is done

--- 15-Incrementing_Counter/js/main.js
+++ 15-Incrementing_Counter/js/main.js
@@ -1,1 +1,21 @@
+const counters = document.querySelectorAll(".counter")
 
+counters.forEach(counter => {
+    counter.innerText = "0"
+
+    const updateCounter = () => {
+        const target = +counter.dataset.target
+        const c = +counter.innerText
+
+        const increment = target / 150
+    
+        if (c < target){
+            counter.innerText = `${Math.ceil(c + increment)}`
+            setTimeout(updateCounter, 1)
+        } else{
+            counter.innerText = target
+        }
+    }
+
+    updateCounter()
+})
