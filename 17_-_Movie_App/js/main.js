f73370a49e21c2137d1a5a74bbabe298d1c68da4
Leonardo N Ghiggino <lghiggino@gmail.com>
2021-01-26 11:31:46 -0300

    done

--- 17_-_Movie_App/js/main.js
+++ 17_-_Movie_App/js/main.js
@@ -1,6 +1,12 @@
 //constants
-const input = document.querySelector("input")
+const form = document.querySelector("#form")
+const search = document.querySelector("#search")
 const main = document.querySelector("main")
+const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=4ba3c3e2200f3937e93cfe0b4f6d6ea8&page=1";
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280"
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?api_key=4ba3c3e2200f3937e93cfe0b4f6d6ea8&query=";
+
+
 
 //on enter in the search
 
@@ -10,13 +16,65 @@ const main = document.querySelector("main")
 
 //fetch the APIAdress
 
-let APIAdress = "https://api.themoviedb.org/3/movie/550?api_key=4ba3c3e2200f3937e93cfe0b4f6d6ea8";
-fetch(APIAdress)
-    .then(res =>  res.json())
-    .then(data => {
-        console.log(data);
-        //  create the cards
+
+// fetch(APIAdress)
+//     .then(res =>  res.json())
+//     .then(data => {
+//         console.log(data);
+//         //  create the cards
+//     })
+//     .catch(error => {
+//         console.log(error)
+//     })
+
+
+
+getMovies(API_URL)
+
+//with async await
+async function getMovies(url){
+    const res = await fetch(url)
+    const data = await res.json()
+    showMovies(data.results)
+}
+
+function showMovies(movies){
+    main.innerHTML = "";
+    movies.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie
+
+        const movieElement = document.createElement("div");
+        movieElement.classList.add("movie")
+        movieElement.innerHTML = `
+            <img src="${IMG_PATH + poster_path}" alt="" class="src">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                <p>${overview}</p>
+            </div>
+        `
+        main.appendChild(movieElement)
     })
-    .catch(error => {
-        console.log(error)
-    })
+}
+
+function getClassByRate(vote){
+    if (vote >= 8){
+        return "green";
+    } else if (vote >= 5){
+        return "orange";
+    } else return "red";
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const searchTerm = search.value
+    if (searchTerm && searchTerm !== ""){
+        getMovies(SEARCH_API+searchTerm)
+        search.value = ""
+    }else{
+        window.location.reload();
+    }
+})
